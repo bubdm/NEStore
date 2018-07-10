41a369c4d3fb92a78c8fe909e1f2c2b614d7fe60
Paolo Lombardo <paolo.lombardo@deltatre.com>
2018-07-10 11:19:57 +0200

    updated mongo drivers to 2.7 (tested with mongo 3.4 and 4.0)

--- NEStore.MongoDb.Tests/MongoDbSerializationTests.cs
+++ NEStore.MongoDb.Tests/MongoDbSerializationTests.cs
@@ -9,6 +9,7 @@ namespace NEStore.MongoDb.Tests
 		[Theory]
 		[InlineData("String", typeof(string))]
 		[InlineData("Dictionary<String,Object>", typeof(Dictionary<string,object>))]
+		[InlineData("List<Dictionary<String,Object>>", typeof(List<Dictionary<string, object>>))]
 		[InlineData("Nullable<Int32>", typeof(int?))]
 		public void GetDiscriminatorName(string exceptedName, Type type)
 		{
