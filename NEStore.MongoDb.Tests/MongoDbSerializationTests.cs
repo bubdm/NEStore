9fbbdeecbece1185af13e54fd2306f79b7db8f7c
Paolo Lombardo <paolo.lombardo@deltatre.com>
2018-07-11 16:58:30 +0200

    added small unit test to check mongo serialization

--- NEStore.MongoDb.Tests/MongoDbSerializationTests.cs
+++ NEStore.MongoDb.Tests/MongoDbSerializationTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using MongoDB.Bson.Serialization;
 using Xunit;
 
 namespace NEStore.MongoDb.Tests
@@ -14,6 +15,43 @@ namespace NEStore.MongoDb.Tests
 		public void GetDiscriminatorName(string exceptedName, Type type)
 		{
 			Assert.Equal(exceptedName, MongoDbSerialization.GetDiscriminatorName(type));
+		}
+
+		[Fact]
+		public void Register()
+		{
+			// It should not fire an exception
+			MongoDbSerialization.Register(typeof(DerivedClass));
+			//var type = typeof(DerivedClass);
+
+			//if (BsonClassMap.IsClassMapRegistered(type))
+			//	return;
+
+			//var cm = new BsonClassMap(type);
+			//cm.AutoMap();
+
+			//BsonClassMap.RegisterClassMap(cm);
+		}
+
+		//public abstract class UltraBaseClass
+		//{
+		//}
+
+		public abstract class BaseClass //: UltraBaseClass
+		{
+			public Guid Id { get; protected set; }
+
+			protected BaseClass(Guid id)
+			{
+				Id = id;
+			}
+		}
+
+		public class DerivedClass : BaseClass
+		{
+			public DerivedClass(Guid id) : base(id)
+			{
+			}
 		}
 	}
 }
